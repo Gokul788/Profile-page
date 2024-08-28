340a92d218c1052a6b9825cbfb2dd7d9caa1f607
gokul788 <gokul774777@gmail.com>
2024-08-28 19:10:37 +0530

    trial version

---
+++ function.js
@@ -0,0 +1,9 @@
+let menubar = document.getElementById("menu-bar");
+var navbar = document.querySelector(".navbar");
+
+function sidebar(){
+    navbar.style.right = "0"
+}
+function Cancel(){
+    navbar.style.right = "-50%"
+}
